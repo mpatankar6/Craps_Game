0bfe8514f31886a9d84c8d5f5f2f4bf453bc6a3e
student <mpatankar06@gmail.com>
2021-11-04 11:54:44 -0400

    ee oo aa ee oo aa lolzolozlozlz

---
+++ src/CrapsGame.java
@@ -0,0 +1,38 @@
+// Implements the game of Craps logic
+
+public class CrapsGame
+{
+    private int point = 0;
+
+    /**
+     *  Calculates the result of the next dice roll in the Craps game.
+     *  The parameter total is the sum of dots on two dice.
+     *  point is set to the saved total, if the game continues,
+     *  or 0, if the game has ended.
+     *  Returns 1 if player won, -1 if player lost,
+     *  0 if player continues rolling.
+     */
+    public int processRoll(int total)
+    {
+
+        if (total == 2 || total == 3 || total == 12 && point == 0) {
+            return 0;
+        } else if (total == 7 || total == 11 && point == 0) {
+            return 1;
+        } else {
+            if()
+        }
+    }
+
+
+        return result;
+    }
+
+    /**
+     *  Returns the saved point
+     */
+    public int getPoint()
+    {
+        return point;
+    }
+}
---
+++ src/Die.java
@@ -0,0 +1,8 @@
+//public class Die {
+//    public static void main(String[] args) {
+//        // TODO Auto-generated method stub
+//        Die die = new Die();
+//        die.roll();
+//        System.out.println(die.getNumDots());
+//    }
+//}
